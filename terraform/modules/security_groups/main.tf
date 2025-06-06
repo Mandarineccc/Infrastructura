226c73e881e66fdf4a0ee1d31d49ef80b8dba4ff
Mickey <skakun.90bk@gmail.com>
2025-06-06 11:31:02 +0300

    fix: ALB health-check ingress for Yandex Cloud and cleanup security group rules

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -3,32 +3,33 @@ resource "yandex_vpc_security_group" "bastion_sg" {
   network_id = var.network_id
 
   egress {
-    protocol = "TCP"
+    protocol = "tcp"
     port     = 0
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    protocol = "TCP"
+    protocol = "tcp"
     port     = 22
     v4_cidr_blocks = ["${var.bastion_ip}/32"]
   }
 }
 
 resource "yandex_vpc_security_group" "web_sg" {
-  name   = "web-sg"
+  name       = "web-sg"
   network_id = var.network_id
 
   egress {
-    protocol = "TCP"
-    port     = 0
+    protocol       = "tcp"
+    port           = 0
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    protocol = "TCP"
-    port     = 80
-    v4_cidr_blocks = ["0.0.0.0/0"]
+    protocol       = "tcp"
+    port           = 80
+    v4_cidr_blocks = ["0.0.0.0/0", "198.18.235.0/24"]
+    description    = "Allow HTTP + ALB health checks"
   }
 }
 
@@ -51,7 +52,7 @@ resource "yandex_vpc_security_group" "kibana" {
   name      = "kibana-sg"
   network_id = var.network_id
   ingress {
-    protocol = "TCP"
+    protocol = "tcp"
     port           = 5601
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
@@ -65,7 +66,7 @@ resource "yandex_vpc_security_group" "elasticsearch" {
   name      = "elasticsearch-sg"
   network_id = var.network_id
   ingress {
-    protocol = "TCP"
+    protocol = "tcp"
     port           = 9200
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
@@ -80,14 +81,14 @@ resource "yandex_vpc_security_group" "zabbix_sg" {
   network_id = var.network_id
 
   ingress {
-    protocol = "TCP"
+    protocol = "tcp"
     description    = "Zabbix frontend"
     port           = 80
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    protocol = "TCP"
+    protocol = "tcp"
     description    = "Zabbix server port"
     port           = 10051
     v4_cidr_blocks = ["0.0.0.0/0"]
